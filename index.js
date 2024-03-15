80befff95a6657d54e663227474588a738fdb8a7
zainabmirza-eng <zainabmirzafatah99@gmail.com>
2024-03-15 22:44:39 +0300

    added

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+const leastFavoriteCustomer = 'alice';
+
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+// console.log(upperCaseCustomerName());
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'char';
+}
+
